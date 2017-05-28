7d57652c2e4f5d6280cfddc9d86258d3012a33b6
root <fernando.bac03@hotmail.com>
2017-05-28 19:14:45 +0200

    fixing problems in stage 1

--- ckanext/dictionary/controller.py
+++ ckanext/dictionary/controller.py
@@ -280,7 +280,7 @@ class DDController(BaseController):
         #if request.method== 'POST':                                           cambie aqui
         #    print("!!!!!!!!!!!!!!!!!!1 POsted FROM EXTENSION!!!!!!!!!!!1")    cambie aqui
             #print(request.params.get())
-        #c.link = str("/dataset/dictionary/new_dict/"+"prueba")
+        c.link = str("/dataset/dictionary/new_dict/"+"prueba")
 	return render("package/new_data_dict.html",extra_vars={'package_id':'prueba'})
 
 
