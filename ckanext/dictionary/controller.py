d5d9bca0a9f6ee2d9aab0726abdc0947bcd1ecd9
root <fernando.bac03@hotmail.com>
2017-05-28 19:27:03 +0200

    review c.link parameter in finaldict

--- ckanext/dictionary/controller.py
+++ ckanext/dictionary/controller.py
@@ -277,9 +277,9 @@ class DDController(BaseController):
 
 
 
-        #if request.method== 'POST':                                           cambie aqui
-        #    print("!!!!!!!!!!!!!!!!!!1 POsted FROM EXTENSION!!!!!!!!!!!1")    cambie aqui
-            #print(request.params.get())
+        if request.method== 'POST':                                           
+            print("!!!!!!!!!!!!!!!!!!1 POsted FROM EXTENSION!!!!!!!!!!!1")    
+            print(request.params.get())
         c.link = str("/dataset/dictionary/new_dict/"+"prueba")
 	return render("package/new_data_dict.html",extra_vars={'package_id':'prueba'})
 
