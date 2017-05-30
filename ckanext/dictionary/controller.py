78fcfe39b9c863c0e275729d613a30eaf6ffd2b6
root <fernando.bac03@hotmail.com>
2017-05-30 13:04:06 +0200

    deleting obsolete lines

--- ckanext/dictionary/controller.py
+++ ckanext/dictionary/controller.py
@@ -8,15 +8,11 @@ from urllib import urlencode
 import datetime
 import mimetypes
 import cgi
-
-
 import ckan.logic as logic
 import ckan.lib.base as base
 import ckan.lib.maintain as maintain
 import ckan.lib.i18n as i18n
 import ckan.lib.navl.dictization_functions as dict_fns
-#import ckan.lib.accept as accept
-#import ckan.lib.helpers as h
 import ckan.model as model
 import ckan.lib.datapreview as datapreview
 import ckan.lib.plugins
@@ -26,10 +22,7 @@ import ckan.lib.render
 
 from ckan.common import config
 import ckan.controllers.package as pkgcontroller
-#import ckan.controllers.package as pkgcontroller
 
-#render = ckan.lib.base.render
-#from home import CACHE_PARAMETERS
 
 log = logging.getLogger(__name__)
 
@@ -39,7 +32,7 @@ pkggg = pkgcontroller.PackageController()
 render = base.render
 abort = base.abort
 redirect = h.redirect_to
-c.selected_pkg = "pruebacontrolador"
+
 
 NotFound = logic.NotFound
 NotAuthorized = logic.NotAuthorized
@@ -54,14 +47,6 @@ flatten_to_string_key = logic.flatten_to_string_key
 lookup_package_plugin = ckan.lib.plugins.lookup_package_plugin
 
 get_action = logic.get_action
-#3333333333333333333333333333333333333
-
-
-
-
-#333333333333333333333333333333333333333
-
-
 
 
 def _encode_params(params):
@@ -297,48 +282,11 @@ class DDController(BaseController):
 	return render("package/new_data_dict.html",extra_vars={'package_id':variable})
 
 
-#    def edit_dictionary(self, id, data=None, errors=None):
-#        context = {'model': model, 'session': model.Session,
-#                   'user': c.user or c.author, 'for_view': True,
-#                   'auth_user_obj': c.userobj, 'use_cache': False}
-#	resource_ids = None
-#        try:
-#	    meta_dict = {'resource_id': '_table_metadata'}
-#	    tables = get_action('datastore_search')(context,meta_dict)
-#	    for t in tables['records']:
-#	        print(t['name'])
-#	        if t['name'] == "data_dict":
-#		    resource_ids = t['alias_of']
-#	except:
-#	    resource_ids == None
-#	if resource_ids == None:
-#	    create = {'resource':{'package_id':id},'aliases':'data_dict','fields':[{'id':'package_id', 'type':'text'},{'id':'id','type':'int4'},{'id':'title','type':'text'},{'id':'field_name','type':'text'},{'id':'format','type':'text'},{'id':'description','type':'text'}],'primary_key':['id']}
-#	    get_action('datastore_create')(context,create)
-#	    print("CREATE TABLE !!!!!!!!!!!!!!!!!!!!!!!IDID")
-#            meta_dict = {'resource_id': '_table_metadata'}
-#            tables = get_action('datastore_search')(context,meta_dict)
-#            for t in tables['records']:
-#                print(t['name'])
-#                if t['name'] == "data_dict":
-#                    resource_ids = t['alias_of']
-#	data_dict_dict = {'resource_id': resource_ids,'filters': {'package_id':id},'sort':['id']}
-#        try:
-#	    print("EXTENSION LOCALLLLLLLLLLLLLLLLLLLLLLLL")
-#            pkg_data_dictionary = get_action('datastore_search')(context, data_dict_dict)
-#            #print(pkg_data_dictionary['records'])
-#            c.pkg_data_dictionary = pkg_data_dictionary['records']
-#	    c.link = str("/dataset/dictionary/new_dict/"  + id)
-#            c.pkg_dict = get_action('package_show')(context, {'id':id})
-#            c.pkg = context['package']
-#        except NotFound:
-#            abort(404, _('Dataset not found'))
-#        except NotAuthorized:
-#            abort(401, _('Unauthorized to read dataset %s') % id)
-#        return render("package/edit_data_dict.html",extra_vars={'package_id':id})
-
     def redirectSecond(self, id, data=None, errors=None):
         return render("package/new_resource.html")
-    def new_data_dictionary(self, id):
+   
+
+ def new_data_dictionary(self, id):
         
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!IN THE EXTENTION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         if request.method == 'POST':
@@ -543,264 +491,6 @@ class DDController(BaseController):
 
 
 
-
-
-
-
-
-
-
-##### documentado desde aqui    ####
-
-
-#    def new_resource_ext(self, id, data=None, errors=None, error_summary=None):
-#        ''' FIXME: This is a temporary action to allow styling of the
-#        forms. '''
-#	c.linkResource = str("/dataset/new_resource/"+id)
-#	print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IN NEW_RESOURCE_EXT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-#        if request.method == 'POST' and not data:
-#            save_action = request.params.get('save')
-#            #if save_action == 'go-datadict':
-#                #redirect(h.url_for(controller='package', action='addDictionary'))
-#            data = data or \
-#                clean_dict(dict_fns.unflatten(tuplize_dict(parse_params(
-#                                                           request.POST))))
-#            # we don't want to include save as it is part of the form
-#            del data['save']
-#            #if 'id' in data.keys()and save_action=="go-dataset-final":
-#            #print("Id found","and the path is: ",request.path)
-#            resource_id = data['id']
-#            #redirect(h.url_for(controller='package',action='read',id=id))
-#            #request.path.split("/")[2]))
-#            #else:
-#            #print("In else part",id," and the request path is: ", request.path)
-#            #if save_action == 'go-dataset-final':
-#            #redirect(h.url_for(controller='package',action='new_resource',id=id))
-#            #request.path.split("/")[2]))#
-#
-#            del data['id']
-#
-#            context = {'model': model, 'session': model.Session,
-#                       'user': c.user or c.author, 'auth_user_obj': c.userobj}
-#
-#            # see if we have any data that we are trying to save
-
-#            data_provided = False
-#            for key, value in data.iteritems():
-#                if ((value or isinstance(value, cgi.FieldStorage))
-#                        and key != 'resource_type'):
-#                    data_provided = True
-#                    break
-#            if not data_provided and save_action != "go-dataset-complete":
-#                if save_action == 'go-dataset':
-#                    # go to final stage of adddataset
-#                    redirect(h.url_for(controller='package',
-#                                       action='edit', id=id))
-#                # see if we have added any resources
-#                try:
-#                    data_dict = get_action('package_show')(context, {'id': id})
-#                except NotAuthorized:
-#                    abort(401, _('Unauthorized to update dataset'))
-#                except NotFound:
-#                    abort(404, _('The dataset {id} could not be found.'
-#                                 ).format(id=id))
-#                if not len(data_dict['resources']):
-#                    # no data so keep on page
-#                    msg = _('You must add at least one data resource')
-#                    # On new templates do not use flash message
-#                    if g.legacy_templates:
-#                        h.flash_error(msg)
-#                        redirect(h.url_for(controller='package',
-#                                           action='new_resource', id=id))
-#                    else:
-#                        errors = {}
-#                        error_summary = {_('Error'): msg}
-#                        return self.new_resource_ext(id, data, errors,
-#                                                 error_summary)
-#                # XXX race condition if another user edits/deletes
-#                data_dict = get_action('package_show')(context, {'id': id})
-#                get_action('package_update')(
-#                    dict(context, allow_state_change=True),
-#                    dict(data_dict, state='active'))
-#                redirect(h.url_for(controller='package',
-#                                   action='read', id=id))
-
-#            data['package_id'] = id
-#            try:
-#                if resource_id:
-#                    data['id'] = resource_id
-#                    get_action('resource_update')(context, data)
-#                else:
-#                    get_action('resource_create')(context, data)
-#            except ValidationError, e:
-#                errors = e.error_dict
-#                error_summary = e.error_summary
-#                return self.new_resource(id, data, errors, error_summary)
-#            except NotAuthorized:
-#                abort(401, _('Unauthorized to create a resource'))
-#            except NotFound:
-#                abort(404, _('The dataset {id} could not be found.'
-#                             ).format(id=id))
-#            if save_action == 'go-metadata':
-#                # XXX race condition if another user edits/deletes
-#                data_dict = get_action('package_show')(context, {'id': id})
-#                get_action('package_update')(
-#                    dict(context, allow_state_change=True),
-#                    dict(data_dict, state='active'))
-#                redirect(h.url_for(controller='package',
-#                                   action='read', id=id))
-#            elif save_action == 'go-datadict':
-#                #if not len(data_dict['resources']):
-#                    # no data so keep on page
-#                    #msg = _('You must add at least one data resource')
-#                    # On new templates do not use flash message
-#                    #if g.legacy_templates:
-#                        #h.flash_error(msg)
-#                        #redirect(h.url_for(controller='package',action='new_resource', id=id))
-#                    #else:
-#                        #errors = {}
-#                        #error_summary = {_('Error'): msg}
-#                        #return self.new_resource(id, data, errors,
-#                                                 #error_summary)
-#                print('save action was go-datadict in the exntenstion NEEWWWW!!!!!!!!!!!')
-#                redirect(h.url_for(str("/dataset/dictionaryy/add/"+id)))
-#		#redirect(h.url_for(controller='package', action='finaldict', id=id))
-#            elif save_action == 'go-dataset':
-#                # go to first stage of add dataset
-#                redirect(h.url_for(controller='package',
-#                                   action='edit', id=id))
-#            elif save_action == 'go-dataset-complete':
-#                # go to first stage of add dataset
-#                redirect(h.url_for(controller='package',
-#                                   action='read', id=id))
-#            else:
-#                # add more resources
-#                redirect(h.url_for(controller='package',
-#                                   action='new_resource', id=id))
-
-#        # get resources for sidebar
-#        context = {'model': model, 'session': model.Session,
-#                   'user': c.user or c.author, 'auth_user_obj': c.userobj}
-#        try:
-#            pkg_dict = get_action('package_show')(context, {'id': id})
-#        except NotFound:
-#            abort(404, _('The dataset {id} could not be found.').format(id=id))
-#        try:
-#            check_access(
-#                'resource_create', context, {"package_id": pkg_dict["id"]})
-#        except NotAuthorized:
-#            abort(401, _('Unauthorized to create a resource for this package'))
-
-#        package_type = pkg_dict['type'] or 'dataset'
-
-#        errors = errors or {}
-#        error_summary = error_summary or {}
-#        vars = {'data': data, 'errors': errors,
-#                'error_summary': error_summary, 'action': 'new',
-#                'resource_form_snippet': self._resource_form(package_type),
-#                'dataset_type': package_type}
-#        vars['pkg_name'] = id
-#        # required for nav menu
-#        vars['pkg_dict'] = pkg_dict
-#        template = 'package/new_resource_not_draft.html'
-#        if pkg_dict['state'].startswith('draft'):
-#            vars['stage'] = ['complete', 'active']
-#            template = 'package/new_resource.html'
-#        return render(template, extra_vars=vars)
-
-#    def dictionary(self, id):
-#        context = {'model': model, 'session': model.Session,
-#                   'user': c.user or c.author, 'for_view': True,
-#                   'auth_user_obj': c.userobj, 'use_cache': False}
-#        data_dict = {'id': id}
-#        try:
-#	    print("here!!!!!!!!1")
-#	    #tem = get_action('package_show')(context, data_dict)
-#	    #print(tem)
- #           c.pkg_dict = get_action('package_show')(context, data_dict)
- #           dataset_type = c.pkg_dict['type'] or 'dataset'
- #       except NotFound:
- #           abort(404, _('Dataset not found'))
- #       except NotAuthorized:
- #           abort(401, _('Unauthorized to read dataset %s') % id)
-#
- #       #if request.method == 'POST':
- #           #new_group = request.POST.get('group_added')
- #           #if new_group:
- #               #data_dict = {"id": new_group,
- #                            #"object": id,
- #                            #"object_type": 'package',
- #                            #"capacity": 'public'}
- #               #try:
-#                    #get_action('member_create')(context, data_dict)
-#                #except NotFound:
-#                    #abort(404, _('Group not found'))
-#
-#            #removed_group = None
-#            #for param in request.POST:
-#                #if param.startswith('group_remove'):
-#                    #removed_group = param.split('.')[-1]
-#                    #break
-#            #if removed_group:
-#                #data_dict = {"id": removed_group,
-#                             #"object": id,
-#                             #"object_type": 'package'}
-
-#                #try:
-#                    #get_action('member_delete')(context, data_dict)
-#                #except NotFound:
-#                    #abort(404, _('Group not found'))
-#            #redirect(h.url_for(controller='package',
-#                               #action='groups', id=id))
-
-#        #context['is_member'] = True
-#        #users_groups = get_action('group_list_authz')(context, data_dict)
-#
-#        #pkg_group_ids = set(group['id'] for group
-#                            #in c.pkg_dict.get('groups', []))
-#        #user_group_ids = set(group['id'] for group
-#                             #in users_groups)
-#
-#        #c.group_dropdown = [[group['id'], group['display_name']]
-#                            #for group in users_groups if
-#                            #group['id'] not in pkg_group_ids]
-
-#        #for group in c.pkg_dict.get('groups', []):
-#            #group['user_member'] = (group['id'] in user_group_ids)
-
-#        context = {'model': model, 'session': model.Session,
-#                   'user': c.user or c.author, 'for_view': True,
-#                   'auth_user_obj': c.userobj, 'use_cache': False}
-#        resource_ids = None
-#        try:
-#            meta_dict = {'resource_id': '_table_metadata'}
-#            tables = get_action('datastore_search')(context,meta_dict)
-#            for t in tables['records']:
-#                print(t['name'])
-#                if t['name'] == "data_dict":
-#                    resource_ids = t['alias_of']
-#	except:
-#	    resource_ids = None
-#        if resource_ids == None:
-#            create = {'resource':{'package_id':id},'aliases':'data_dict','fields':[{'id':'package_id','type':'text'},{'id':'id','type':'int4'},{'id':'title','type':'text'},{'id':'field_name','type':'text'},{'id':'format','type':'text'},{'id':'description','type':'text'}],'primary_key':['id']}
-#            get_action('datastore_create')(context,create)
-#	    print("CREATE TABLE !!!!!!!!!!!!!!!!!!!!!!!")
-#            meta_dict = {'resource_id': '_table_metadata'}
-#            tables = get_action('datastore_search')(context,meta_dict)
-#            for t in tables['records']:
-#                print(t['name'])
-#                if t['name'] == "data_dict":
-#                    resource_ids = t['alias_of']
-#        data_dict_dict = {'resource_id': resource_ids,'filters': {'package_id':id},'sort':['id']}
-#        try:
-#            pkg_data_dictionary = get_action('datastore_search')(context, data_dict_dict)
-#            print(pkg_data_dictionary['records'])
-#            c.pkg_data_dictionary = pkg_data_dictionary['records']
-#        except NotFound:
-#            abort(404, _('Dataset not found'))
-#        except NotAuthorized:
-#            abort(401, _('Unauthorized to read dataset %s') % id)
-#        return render('package/dictionary_display.html',{'dataset_type': dataset_type})
 
 
 
